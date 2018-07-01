16ccf3190116e781ed7ff419a8ea788e44c63a5d
Simon Fontana Oscarsson <sfo_93@hotmail.com>
2018-07-01 11:13:19 +0200

    Compare returns string

--- compare_yamls.py
+++ compare_yamls.py
@@ -3,38 +3,44 @@
 from yaml import load, dump
 
 
-def traverse_compare_vals(val1, val2):
+def traverse_compare_vals(val1, val2, parent):
     difs = []
     if type(val1) is dict:
-        difs = traverse_compare_dicts(val1, val2)
+        difs = traverse_compare_dicts(val1, val2, parent)
     elif type(val1) is list:
-        difs = traverse_compare_lists(val1, val2)
+        difs = traverse_compare_lists(val1, val2, parent)
     else:
         if val1 != val2:
-            difs.append(val2)
+            difs.append(parent + "=" + str(val2))
     return difs
 
 
-def traverse_compare_dicts(d1, d2):
+def traverse_compare_dicts(d1, d2, parent):
     difs = []
     for key, val in d1.items():
-        d = traverse_compare_vals(val, d2[key])
+        if parent == "":
+            p = key
+        else:
+            p = parent + '.' + key
+        d = traverse_compare_vals(val, d2[key], p)
         if d:
             difs.extend(d)
     return difs
 
 
-def traverse_compare_lists(l1, l2):
+def traverse_compare_lists(l1, l2, parent):
     difs = []
     for i in l1:
-        d = traverse_compare_vals(i, l2[l1.index(i)])
+        index = l1.index(i)
+        p = parent + '[' + str(index) + '].'
+        d = traverse_compare_vals(i, l1[l1.index(i)], p)
         if d:
             difs.extend(d)
     return difs
 
 
 def compare_yamls(y1,y2):
-    return traverse_compare_dicts(y1, y2)
+    return traverse_compare_dicts(y1, y2, "")
 
 
 if __name__ == '__main__':
@@ -44,6 +50,3 @@ if __name__ == '__main__':
     data2 = load(f2)
     differences = compare_yamls(data1, data2)
     print(differences)
-
-    if data2['metadata']['name'] in differences:
-        print('Excellent!')
